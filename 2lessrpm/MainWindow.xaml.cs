8b7c2e3779ef6cfdd84b45a0d42f8941a8d6904b
Александр <@MEGABOOK>
2023-09-18 13:46:07 +0700

    Добавьте файлы проекта.

---
+++ 2lessrpm/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _2lessrpm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<string> MyList;
+        public MainWindow()
+        {
+            InitializeComponent();
+            MyList = new List<string>()
+            {
+                "str_1",
+                "str_2",
+                "str_3",
+                "str_4",
+            };
+            ListBoxView.ItemsSource = MyList;
+            ListBoxView.FontSize = 45;
+            List<User> user = new List<User>()
+            {
+                new User(0,"ivan", "jhgjhg@gmail.com"),
+                new User(1,"Semen", "jhgj@gmail.com"),
+                new User(2,"Volod", "slkjk@gmail.com"),
+            };
+            MyListView.ItemsSource = user;
+            MyListView.FontSize = 30;
+            table.ItemsSource = user;
+        }
+        public void AddItemInList(object sender, RoutedEventArgs e)
+        {
+            MyList.Add(TextBoxAddList.Text);
+            ListBoxView.Items.Refresh();
+        }
+
+        private void MyListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var item =  MyListView.SelectedItem as User;
+            TextID.Text = Convert.ToString(item.id);
+            TextName.Text = item.name;
+            TextEmail.Text = item.Email;
+
+        }
+    }
+}
---
+++ 2lessrpm/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2lessrpm
+{
+    partial class User
+    {
+        public User(int id, string? name, string email) 
+        {
+            this.id = id;
+            this.name = name;
+            Email = email;
+        }
+        public int id { get; set; }
+        public string? name { get; set; }
+        public string Email { get; set; }
+
+    }
+}
